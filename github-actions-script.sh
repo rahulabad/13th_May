6a27673f7b2c9330662786fa0471bc534d5de137
Rahul <42180424+rahulabad@users.noreply.github.com>
2022-05-13 10:08:51 +0530

    Create github-actions-script.sh

---
+++ github-actions-script.sh
@@ -0,0 +1,15 @@
+          echo What is in this directory?
+          echo
+          ls -a
+          echo
+          echo Is java installed?
+          echo
+          java -version
+          echo
+          echo Who is running the script?
+          echo
+          whoami
+          echo
+          echo What is Environment?
+          echo
+          env
